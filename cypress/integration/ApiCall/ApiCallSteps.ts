c87e096781bd086815dee2bcfd5d2abdbfbe7eeb
Oliver Juráček <oliver.juracek@avitech.sk>
2022-01-27 15:50:24 +0100

    updated /support and ApiCallSteps.ts

---
+++ cypress/integration/ApiCall/ApiCallSteps.ts
@@ -0,0 +1,26 @@
+/// <reference types="cypress" />
+
+beforeEach(() => {
+
+})
+
+
+
+it('call get-all-users', () => {
+
+    cy.request({
+        method: 'GET',
+        url: '/get-all-users'
+    }).as('userDetails')
+})
+
+
+// cy.get('bodys'[0]).should('have.length', 3)
+
+const uNames = ['Ivana', 'Dominik', 'Ivana']
+
+// cy.wait('@userDetails').then( user => {
+//     const userNames = Cypress._.map(user.response.body.name)
+//     expect(userNames).to.deep.eq(uNames)
+// })
+
--- dist/cypress/integration/ApiCall/apiCallSteps.js
+++ dist/cypress/integration/ApiCall/apiCallSteps.js
@@ -1,1 +1,16 @@
 "use strict";
+/// <reference types="cypress" />
+beforeEach(() => {
+});
+it('call get-all-users', () => {
+    cy.request({
+        method: 'GET',
+        url: '/get-all-users'
+    }).as('userDetails');
+});
+// cy.get('bodys'[0]).should('have.length', 3)
+const uNames = ['Ivana', 'Dominik', 'Ivana'];
+// cy.wait('@userDetails').then( user => {
+//     const userNames = Cypress._.map(user.response.body.name)
+//     expect(userNames).to.deep.eq(uNames)
+// })
