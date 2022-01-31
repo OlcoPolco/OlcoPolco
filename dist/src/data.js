e2f64a22c072244897ef7858421d783cf8a7e737
Oliver Juráček <oliver.juracek@avitech.sk>
2022-01-31 08:17:19 +0100

    no app

--- dist/src/data.js
+++ dist/src/data.js
@@ -6,13 +6,14 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.useDataSchema = void 0;
 const mongoose_1 = __importDefault(require("mongoose"));
 // MongoDB connect
-// mongoose.connect('mongodb://localhost:27017/local', function(err) {
-//     if (err) {
-//         console.log('Not connected to the database: ' + err);
-//     } else {
-//         console.log('Successfully connected to MongoDB')
-//     }
-// });
+mongoose_1.default.connect('mongodb://localhost:27017/local', function (err) {
+    if (err) {
+        console.log('Not connected to the database: ' + err);
+    }
+    else {
+        console.log('Successfully connected to MongoDB');
+    }
+});
 // Data schema
 exports.useDataSchema = new mongoose_1.default.Schema({
     name: { type: String, required: true },
